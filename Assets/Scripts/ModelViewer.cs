d0734f4f704f935d346402c7a4f41a836b74a42d
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-06-14 20:02:02 +0200

    Added taking screenshots

--- Assets/Scripts/ModelViewer.cs
+++ Assets/Scripts/ModelViewer.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ModelViewer : MonoBehaviour
 {
     private List<GameObject> _models = new List<GameObject>();
     private int _currentModel = 0;
+    private string _outputPath = string.Empty;
+
+    private void Start()
+    {
+        _outputPath = Application.dataPath + "/Output";
+    }
 
     public void SetModels( List<GameObject> models )
     {
@@ -38,6 +45,12 @@ public class ModelViewer : MonoBehaviour
 
     public void TakePhoto()
     {
-        // TODO
+        if ( !Directory.Exists( _outputPath ) )
+        {
+            Directory.CreateDirectory( _outputPath );
+        }
+
+        DateTime dateTime = DateTime.Now;
+        ScreenCapture.CaptureScreenshot( _outputPath + $"/{_models[ _currentModel ].name}_{dateTime:yyMMddHHmmss}.png" );
     }
 }
